43b9da223d73ba0cb21ef65f73126dfd8953fc87
Victory <git@dfhu.org>
2017-04-02 19:16:22 -0500

    remove the flex monkey

--- frontvue/src/App.vue
+++ frontvue/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <div class="monkey">The monkey is here</div>
     <router-view></router-view>
   </div>
 </template>
@@ -21,8 +20,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-  .monkey {
-    background-color: rebeccapurple;
-    flex: 1;
-  }
 </style>
