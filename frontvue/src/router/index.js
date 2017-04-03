427efad487cc32f87d5ced0f66643c88ff44a6cb
Victory <git@dfhu.org>
2017-04-03 16:19:12 -0500

    404 page and set titles of pages on route change

--- frontvue/src/router/index.js
+++ frontvue/src/router/index.js
@@ -4,31 +4,49 @@ import Home from '@/components/Home'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Privacy from '@/components/Privacy'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {title: 'Home'}
     },
     {
       path: '/about',
       name: 'about',
-      component: About
+      component: About,
+      meta: {title: 'About'}
     },
     {
       path: '/contact',
       name: 'contact',
-      component: Contact
+      component: Contact,
+      meta: {title: 'Contact Us'}
     },
     {
       path: '/privacy',
       name: 'privacy',
-      component: Privacy
+      component: Privacy,
+      meta: {title: 'Privacy Policy'}
+    },
+
+    // 404 as Last Catch All Path
+    {
+      path: '*',
+      component: NotFound,
+      meta: {title: '404 Not Found'}
     }
   ]
-})
+});
+
+router.afterEach(route => {
+  document.title = route.meta.title + ' | TreasureHuntPWA';
+});
+
+export default router;
