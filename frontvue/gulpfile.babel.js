c97eeed130737ee0dca75282191032818cf1bed3
ColonelBologna <cgraveline15@gmail.com>
2017-04-12 21:45:00 -0400

    added postcss to gulpfile.babel.js

--- frontvue/gulpfile.babel.js
+++ frontvue/gulpfile.babel.js
@@ -1,8 +1,20 @@
 'use strict';
 
 import gulp from 'gulp';
+import postcss from 'gulp-postcss';
 import del from 'del';
 import shell from 'gulp-shell';
+
+gulp.task('css', function() {
+  var postcss = require('gulp-postcss');
+  var sourcemaps = require('gulp-sourcemaps');
+
+  return gulp.src('src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([require('precss'), require('autoprefixer')]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/'));
+});
 
 gulp.task('upgrade', shell.task([
   "yarn"
@@ -31,6 +43,8 @@ gulp.task('generate-service-worker', function(callback) {
   }, callback);
 });
 
-gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {
+  dot: true
+}));
 
 gulp.task('build', ['builddist']);
